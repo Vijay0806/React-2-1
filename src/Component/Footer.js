1f1dd81e9d2a2a2be8690c196f5f6312b3bbd88d
Vijay0806 <vijaym08062001@gmail.com>
2023-05-11 20:11:18 +0530

    second commit

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -5,7 +5,7 @@ export const Footer = () => {
         <div>
 
             <footer class="py-5 bg-dark">
-                <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
+                <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
             </footer>
         </div>
     )
